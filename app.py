64f8f91fcff29339b3acb48d4a83e2aadec1d97e
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-02 18:31:31 +0000

    updating streamlit stuff (finally worked)

---
+++ app.py
@@ -0,0 +1,9 @@
+''' Visualising Irish Charity data using Streamlit '''
+
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+import pydeck as pdk 
+
+st.text("Hello Jack")
