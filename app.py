8f4f0a08214d10db59d845013ed291face3f8cd0
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-06 18:55:54 +0000

    final changes

--- app.py
+++ app.py
@@ -28,10 +28,16 @@ with row1_2:
 def load_data():
     path = './Data/CleanCSV/big_dataset.csv'
     df = pd.read_csv(path).dropna()
+    df['Financial: Gross Income'] = np.ceil(df['Financial: Gross Income'])
+
     return df
 
 data = load_data()
-print(data.columns)
+
+# Create Financial HeatMap dataset
+fin_heat = data[['lng', 'lat', 'Financial: Gross Income']].groupby(['lng']).mean().reset_index()
+fin_heat['Financial: Gross Income'] = np.ceil(fin_heat['Financial: Gross Income']/10000)
+fin_heat = fin_heat.loc[fin_heat.index.repeat(fin_heat['Financial: Gross Income'])]
 
 purposes = sorted(list(data.Purpose.str.split(' ', expand=True).stack().unique()))
 for word in list(purposes):  # iterating on a copy since removing will mess things up
@@ -78,4 +84,52 @@ for i in volunteers_filter:
         i = 'Oct-19'
     data = data[data['Number of Volunteers'].str.contains(i)]
 
-# LAYING OUT THE ChHARTS OF THE APP 
+
+# CHART LAYOUT
+row2_1, row2_2 = st.beta_columns((2,2))
+
+# Define a layer to display on a map
+layer = pdk.Layer(
+    "HexagonLayer",
+    data[['lng', 'lat']],
+    get_position=["lng", "lat"],
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[100, 3000],
+    extruded=True,
+    coverage=1,
+)
+
+view_state = pdk.ViewState(longitude=-6.6564, latitude=53.6055, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36)
+
+def map(data):
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={"latitude": 53.6055, "longitude": -6.6564, "zoom": 6, "pitch": 50,},
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data,
+                get_position=["lng", "lat"],
+                auto_highlight=True,
+                elevation_scale=50,
+                pickable=True,
+                elevation_range=[0, 3000],
+                extruded=True,
+                coverage=1,
+            ),
+        ]
+    ))
+
+with row2_1:
+    st.write("**Number of Charities**")
+    map(data[['lng', 'lat']])
+
+with row2_2:
+    st.write("**Financial Income**")
+    map(fin_heat[["lng", "lat"]])
+
+# FILTERED TABLE LAYOUT
+st.write("**Filtered Table of Charities**")
+st.table(data[['Registered Charity Name', 'Period Start Date', 'Period End Date', 'Financial: Gross Income', 'Financial: Gross Expenditure']])
